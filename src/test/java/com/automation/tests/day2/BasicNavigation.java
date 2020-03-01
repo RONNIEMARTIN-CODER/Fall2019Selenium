2fb2f88a7869b85f1ba291dcdd8618bde66cdcfe
Rmartin40 <ladonmartin86@gmail.com>
2020-03-01 12:56:14 -0600

    Updated changes to basic navigation

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,12 +6,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {  // throws Exception to run Thread.sleep
         // to start selenium script we need:
         // setup webdriver (browser driver) and create webdriver object
         WebDriverManager.chromedriver().setup();
+
         WebDriver driver = new ChromeDriver();
+
         // In Selenium, everything starts from WebDriver interface
+        // ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com");// to open a website
+
+        Thread.sleep(3000);// for demo, wait 3 seconds
+        //method that return page title
+        //you can also see it as tab name, in the browser
+        String title = driver.getTitle(); // returns <title>SOme title </title> text
+
+        String expectedTitle = "Google";// we provide it
+
+        System.out.println("Title is..." + title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("TESTED PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+
+        // MUST BE AT THE END
+        driver.close();// to close browser
+        //browser cannot close itself
     }
 }
