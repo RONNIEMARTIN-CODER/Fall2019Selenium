e9f629cfd85a9bdee4f32ac913736fb441619647
Rmartin40 <ladonmartin86@gmail.com>
2020-03-03 20:40:21 -0600

    FindElementsPractice updated w/getAttribute

--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -3,6 +3,7 @@ package com.automation.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -16,6 +17,7 @@ public class DriverFactory {
      */
     public static WebDriver createADriver(String browserName){
         if (browserName.equalsIgnoreCase("chrome")){
+            
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
         }else {
