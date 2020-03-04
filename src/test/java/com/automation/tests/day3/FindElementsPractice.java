5c7699a4f2c58d92de101b2edefee5160c1405d1
Rmartin40 <ladonmartin86@gmail.com>
2020-03-03 19:18:13 -0600

    FindElementsPractice updated

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -2,17 +2,35 @@ package com.automation.tests.day3;
 
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 //        WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
         WebDriver driver = DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+
+        Thread.sleep(2000);
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com");
+
+        Thread.sleep(2000);
+
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        // when you see type="submit", you can use submit() instead of click()
+        // it make sense to use when click() method doesn't work
+        signUp.click();
+        Thread.sleep(2000);
+
 
 
 
